71e8e3bc95791dacdc40e19ae911f4fff214686c
nitish5511@gmail.com <nitish5511@gmail.com>
2020-10-23 12:44:16 +0530

    first commit

---
+++ src/Basics/Testing.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Eclipse install succesfully with java");
+
+	}
+
+}
