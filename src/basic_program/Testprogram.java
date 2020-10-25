b3e89573b82299321f9572d669ecc2525c6a4300
nitish5511@gmail.com <nitish5511@gmail.com>
2020-10-25 09:04:55 +0530

    test class

---
+++ src/basic_program/Testprogram.java
@@ -0,0 +1,12 @@
+package basic_program;
+
+public class Testprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing class created");
+
+	}
+
+}
